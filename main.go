688733a8191ff03a85861bf5bc2a4b4a881e0f1c
Leonid Perlin <perlinleo@gmail.com>
2021-09-07 21:53:26 +0300

    uniq func

--- main.go
+++ main.go
@@ -25,6 +25,41 @@ func compareTwoString(s string, t string) bool {
 	return s==t;
 }
 
+func uniq(text []string,
+		   count bool,
+		   deleteUnrepeated bool,
+		   caseInsensitive bool,
+		   ignoreFirst int,
+		   ignoreStartSymbols int) string {
+			stringComparator := compareTwoString;
+			if caseInsensitive {
+				stringComparator = strings.EqualFold;
+			}
+			previous := subSentence(text[0],ignoreFirst,ignoreStartSymbols);
+			var counter int;
+			for i := 1; i < len(text); i++ {
+				current := subSentence(text[i],ignoreFirst,ignoreStartSymbols);
+				// fmt.Println(current);
+				if !stringComparator(previous, current) { 
+					if count {
+						fmt.Printf("%d ", counter+1);
+						counter=0;
+					}
+					fmt.Println(text[i-1]);
+					
+				} else if count {
+					counter++;
+				}
+				previous = current;
+			}
+			if(count) { 
+				fmt.Printf("%d ", counter+1)
+			} 
+			fmt.Println(text[len(text)-1]);
+
+			return "";
+		   }
+
 func main() {
 	count := flag.Bool("c", false, "Count repeating strings?");
 	deleteUnrepeated := flag.Bool("d", false, "Delete repeating strings?");
@@ -48,39 +83,15 @@ func main() {
             log.Fatal(err)
         }
     }()
-	stringComparator := compareTwoString;
-	if *caseInsensitive {
-		stringComparator = strings.EqualFold;
-	}
+	
     buf := make([]byte, 32*1024) 
 
     for {
         n, err := file.Read(buf)
 
         if n > 0 {
-            s := strings.Split(string(buf[:n]),"\n")
-			previous := subSentence(s[0],*ignoreFirst,*ignoreStartSymbols);
-			var counter int;
-			for i := 1; i < len(s); i++ {
-				current := subSentence(s[i],*ignoreFirst,*ignoreStartSymbols);
-				// fmt.Println(current);
-				if !stringComparator(previous, current) { 
-					if *count {
-						fmt.Printf("%d ", counter+1);
-						counter=0;
-					}
-					fmt.Println(s[i-1]);
-					
-				} else if *count {
-					counter++;
-				}
-				previous = current;
-			}
-			if(*count) { 
-				fmt.Printf("%d %s", counter+1, s[len(s)-1])
-			} else {
-				fmt.Println(s[len(s)-1]);
-			}
+			s := strings.Split(string(buf[:n]),"\n")
+            uniq(s,*count,*deleteUnrepeated,*caseInsensitive,*ignoreFirst,*ignoreStartSymbols);
         }
 
         if err == io.EOF {
